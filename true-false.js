40d988575d70bc80c5f1927e5d1e5b95c3441ac9
Md Emran Nazir <mdemrannazir9999@gmail.com>
2020-08-11 10:26:50 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,42 @@
+// False value
+    // false,
+    // 0,
+    // "",
+    // undefined,
+    // null,
+    // NaN,
+
+// True vale
+    // true,
+    // ' ',
+    // [],
+    // {}
+
+// const number = 1 // value is true
+// const number1 = 0 // value is false
+
+if (number) {
+    console.log('Condition is true');
+} else {
+    console.log('COndition is false');
+}
+
+// const string = 'Emran Nazir' // ' ' -> value is true
+// const string = '' // value ils false
+
+if (string) {
+    console.log('Condition is true');
+} else {
+    console.log('COndition is false');
+}
+
+// false value
+// const name = null
+// let name
+// const name = undefined
+
+if (name) {
+    console.log('Condition is true');
+} else {
+    console.log('COndition is false');
+}
